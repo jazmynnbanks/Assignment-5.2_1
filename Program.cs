97a32c44619143b8edcbc8f03ad3146b216542bf
jazmy <jazmy@JAZ-LAPTOP>
2025-05-07 11:05:52 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Given a string s consisting of words and spaces, return the length of the last word in the string. A word is a maximal substring consisting of non-space characters only.
+
+static void LastWord()
+{
+
+    string s = "Welcome to the Jungle";
+    int x = s.Length - 1; 
+    int count = 0;
+
+    while (x > 0 && s[x] == ' ')
+    {
+        x--;
+    }
+
+    while (x > 0 && (s[x] != ' '))
+        {
+        count++;
+        x--;
+    }
+    Console.WriteLine(count);
+
+}
+
+LastWord();
+
